1364d92d271a646d6572896759326d14e496fcfd
Petr Reshetin <preshetin@gmail.com>
2024-11-21 19:21:54 +0300

    feat: answer from document

--- app.py
+++ app.py
@@ -4,6 +4,13 @@ import requests
 import os
 from bs4 import BeautifulSoup
 
+from langchain_pinecone import PineconeEmbeddings, PineconeVectorStore
+from langchain_openai import ChatOpenAI
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain import hub
+from pinecone import Pinecone
+
 app = Flask(__name__)
 
 if os.getenv('FLASK_ENV') == 'development':
@@ -77,10 +84,11 @@ def webhook():
         user_message = update['message']['text']
         
         # Get response from OpenAI
-        gpt_response = get_gpt_response(user_message)
+        # gpt_response = get_gpt_response(user_message)
+        response = get_answer_from_document(user_message)
         
         # Send response back to Telegram
-        send_message(chat_id, gpt_response)
+        send_message(chat_id, response)
     
     return '', 200
 
@@ -111,6 +119,49 @@ def send_message(chat_id, text):
         'parse_mode': 'Markdown'  # Optional: use Markdown formatting
     }
     requests.post(url, json=payload)
+
+def get_answer_from_document(message):
+    # Initialize Pinecone
+    pc = Pinecone(api_key=os.environ.get("PINECONE_API_KEY"))
+
+    # Define the index and namespace
+    index_name = "rag-getting-started"
+    namespace = "wondervector5000"
+
+    # Initialize the embeddings
+    model_name = 'multilingual-e5-large'
+    embeddings = PineconeEmbeddings(
+        model=model_name,
+        pinecone_api_key=os.environ.get('PINECONE_API_KEY')
+    )
+
+    # Load the existing vector store
+    docsearch = PineconeVectorStore(
+        index_name=index_name,
+        embedding=embeddings,
+        namespace=namespace
+    )
+
+    # Initialize the retriever
+    retriever = docsearch.as_retriever()
+
+    # Initialize the language model
+    llm = ChatOpenAI(
+        openai_api_key=os.environ.get('OPENAI_API_KEY'),
+        model_name='gpt-4o-mini',
+        temperature=0.0
+    )
+
+    # Create the retrieval chain
+    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+    combine_docs_chain = create_stuff_documents_chain(
+        llm, retrieval_qa_chat_prompt
+    )
+    retrieval_chain = create_retrieval_chain(retriever, combine_docs_chain)
+    
+    answer_with_knowledge = retrieval_chain.invoke({"input": message})
+
+    return answer_with_knowledge['answer']
 
 if __name__ == '__main__':
     app.run(debug=True)
