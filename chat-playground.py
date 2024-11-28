e2a2c4d42175df637bfd1c740efa6200c8187992
Petr Reshetin <preshetin@gmail.com>
2024-11-28 16:08:36 +0300

    prototype tool calling

--- chat-playground.py
+++ chat-playground.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from langchain_pinecone import PineconeEmbeddings, PineconeVectorStore
 from langchain_openai import ChatOpenAI
@@ -10,7 +11,26 @@ from pinecone import Pinecone
 
 load_dotenv()
 
-def get_answer_from_document(message):
+from langchain_core.tools import tool
+
+import requests
+
+@tool
+def get_courses_schedule_from_api():
+    """Get vipassana courses schedule. Если спрашивают, какое расписание на курсе, то этот инструмент не должен вызываться. This tool schould be called when courses schedule is requested. This tool should not be called if a user requests course (singular) schedule."""
+    url = "https://seahorse-app-db78s.ondigitalocean.app/api/courses"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an error for bad responses (4xx or 5xx)
+        return response.json()  # Return the JSON response
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None  # Return None in case of an error
+
+
+@tool
+def get_answer_from_document(message: str) -> str:
+    """Gets answer from meditation children course document. The tool should receive the whole user message in 'message' parameter. This tool should not be called if a user requests courses (plural) schedule. """
     # Initialize Pinecone
     pc = Pinecone(api_key=os.environ.get("PINECONE_API_KEY"))
 
@@ -53,5 +73,52 @@ def get_answer_from_document(message):
 
     return answer_with_knowledge['answer']
 
-res = get_answer_from_document('скинь ссылку на письмо домой')
-print(res)
+# res = get_answer_from_document('скинь ссылку на письмо домой')
+# print(res)
+
+
+tools = [get_answer_from_document, get_courses_schedule_from_api]
+
+tools_mapping = {
+    "get_answer_from_document": get_answer_from_document,
+    "get_courses_schedule_from_api": get_courses_schedule_from_api
+}
+
+llm = ChatOpenAI(model="gpt-4o-mini")
+
+from langchain_core.messages import HumanMessage
+
+# Usage:
+# python chat-playground.py "скинь ссылку на письмо домой"
+# python chat-playground.py "какое расписание на детском курсе"
+# python chat-playground.py "пришли расписание курсов"
+query = sys.argv[1]
+
+print('query', query)
+print('\n\n\n')
+
+messages = [HumanMessage(query)]
+
+llm_with_tools = llm.bind_tools(tools, tool_choice='required')
+
+ai_msg = llm_with_tools.invoke(messages)
+
+messages.append(ai_msg)
+
+
+print('tool calls', ai_msg.tool_calls)
+print('\n\n\n')
+
+for tool_call in ai_msg.tool_calls:
+    selected_tool = tools_mapping.get(tool_call["name"].lower())
+    tool_msg = selected_tool.invoke(tool_call)
+    messages.append(tool_msg)
+
+
+res = llm.invoke(messages)
+
+print('res variable', res)
+
+print('\n\n\n')
+print('final answer', res.content)
+print('\n\n\n')
