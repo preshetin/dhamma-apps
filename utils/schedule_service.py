288835df7c48550a854082e286c3e2c4cee708c6
Peter Reshetin <preshetin@gmail.com>
2025-04-18 09:16:54 +0300

    Update schedule_service.py

--- utils/schedule_service.py
+++ utils/schedule_service.py
@@ -17,15 +17,13 @@ def get_schedule_service(status, course_type='ten-day', location='all', year='cu
             "year": "current",
             "dom_element": "body > div > div > div:nth-child(8) > div:nth-child(2) > table:nth-child(4) > tbody",
         },
-        """
-        {
+        #{
             # second table, dullabha next year
-            "title": "2025 Десятидневные и другие курсы для взрослых",
-            "block": "dullabha",
-            "year": "next",
-            "dom_element": "body > div > div > div:nth-child(8) > div:nth-child(2) > table:nth-child(6) > tbody",
-        },
-        """
+           # "title": "2025 Десятидневные и другие курсы для взрослых",
+          #  "block": "dullabha",
+           # "year": "next",
+          #  "dom_element": "body > div > div > div:nth-child(8) > div:nth-child(2) > table:nth-child(6) > tbody",
+      #  },
         # Separator: Курсы вне центров
         {
             # 3-rd table: ten days, childern, one day,
