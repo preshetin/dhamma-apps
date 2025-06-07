44277318efbed4bab5d2b759209035bee8c24fc1
Petr Reshetin <preshetin@gmail.com>
2025-06-07 22:11:19 +0300

    100 stars payment

--- webhooks/telegram_petyavpn.py
+++ webhooks/telegram_petyavpn.py
@@ -53,11 +53,11 @@ def webhook_petyavpn():
             payload = {
                 'chat_id': chat_id,
                 'title': 'VPN for June, 2025',
-                'description': 'Purchase 1 Telegram Stars',
+                'description': 'Purchase 100 Telegram Stars',
                 'payload': 'stars_payment',
                 # 'provider_token': os.environ.get('TELEGRAM_PAYMENT_TOKEN'),  # You need to set this in environment variables
                 'currency': 'XTR',
-                'prices': [{'label': '1 Stars', 'amount': 1}],  # Amount in cents (1 USD)
+                'prices': [{'label': '100 Stars', 'amount': 100}],  # Amount in cents (1 USD)
                 'need_name': False,
                 'need_phone_number': False,
                 'need_email': False,
