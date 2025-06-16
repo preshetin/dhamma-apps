330e4531110e749b6d4de6d9242b09a73d0c95bf
Petr Reshetin <preshetin@gmail.com>
2025-06-16 19:59:59 +0300

    elif

--- webhooks/telegram_petyavpn.py
+++ webhooks/telegram_petyavpn.py
@@ -121,8 +121,7 @@ def webhook_petyavpn():
             requests.post(url, json=payload)
             # TODO: db: create new chat with chat_id and user info
             # TODO: db: add message from 'user' with text "/start"
-
-        if user_message.lower() == 'оплата':
+        elif user_message.lower() == 'оплата':
             send_invoice(chat_id, 100)
             send_message(chat_id, "Если у вас не получается оплатить (в РФ не работает ApplePay), попробуйте купить звезды через @PremiumBot и вернитесь сюда и нажмите кнопку оплаты.", parse_mode='html')
         else:
